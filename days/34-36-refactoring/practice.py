a1caf28c488f6035b11653ef0c2c38c30b2ec036
Qkessler <qkessler@gmail.com>
2020-02-26 09:09:25 +0100

    Commiting with

--- days/34-36-refactoring/practice.py
+++ days/34-36-refactoring/practice.py
@@ -20,3 +20,10 @@ print(f'{persona} tiene {get_edad(persona)} años')
 # El uno que ponemos detrás de personas es para indicar dónde empieza el index.
 for i, persona in enumerate(personas, 1):
     print(f'Persona {i}: {persona}')
+
+# Para manejar los contextos teníamos varias opciones: Una era usar try-except, pero daba error
+# dejando por ejemplo el fichero abierto. Nos interesa usar with, que cierra automáticamente.
+
+with open('practica.py', 'w') as fichero:
+    fichero.write('')
+    raise Exception
