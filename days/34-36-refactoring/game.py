203ffd972728c42f360d46f9ec8d20d2b24c256a
Enrique Kessler <qkessler@gmail.com>
2020-02-27 11:10:50 +0100

    Commiting starting of refactoring

---
+++ days/34-36-refactoring/game.py
@@ -0,0 +1,138 @@
+import random
+# import sys
+import logbook
+
+
+app_log = logbook.Logger('App')
+
+
+class Roll():
+    def __init__(self, name):
+        self.name = name
+        self.defeat = self.defeat()
+        self.beDefeated = self.beDefeated()
+
+    def defeat(self):
+        if self.name == 'Rock':
+            return 'Scissors'
+        elif self.name == 'Paper':
+            return 'Rock'
+        elif self.name == 'Scissors':
+            return 'Paper'
+        else:
+            print('The roll is not coded yet')
+
+    def beDefeated(self):
+        if self.name == 'Rock':                                                                                                                     
+            return 'Paper'                                                                                                                       
+        elif self.name == 'Paper':                                                                                                                  
+            return 'Scissors'
+        elif self.name == 'Scissors':                                                                                                               
+            return 'Rock'
+        else:
+            print('The roll is not coded yet')
+
+    def __str__(self):
+        return self.name
+
+
+class Player():
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'Player_' + self.name
+
+
+class Game():
+
+    def __init__(self):
+        self.lives = 3
+
+    def main(self):
+        app_log.trace("Initializing setup and gameloop.")
+        self.setup()
+        self.game_loop()
+
+    def setup(self):
+        print('--------------------------')
+        print('Rock, paper, Scissors Game')
+        print('--------------------------')
+        print()
+
+    def game_on(self):
+        return self.lives > 0
+
+    def decrement_lives(self):
+        self.lives -= 1
+
+    def game_loop(self):
+        choices = [
+            Roll("Paper"), Roll("Rock"), Roll("Scissors")
+             ]
+        print('Welcome to the game of RPS!')
+        print('What is your name?')
+        name = input("- ")
+        name.strip()
+        while name == "":
+            name = input("- ").strip()
+            if name == "":
+                app_log.error("User chose a blank name")
+                raise ValueError("ERROR: Name cannot be blank!")
+            
+        app_log.trace("User's name: {}".format(name))
+        print("Hey {}! Welcome to the rps game!".format(name))
+        print('You start with 3 lives.')
+
+        app_log.trace("Entering main loop.")
+        number_choice = 1
+        while self.game_on():
+            machine_choice = random.choice(choices)
+            print('What are you choosing? r, p, s: ')
+            choice = input("- ")
+            app_log.trace("{} user's choice is {}.".format(number_choice, choice))
+
+            if choice == 'r':
+                print('You have chosen Rock!')
+                print("The machine's choice was {}!".format(machine_choice))
+                if machine_choice == "Rock":
+                    result = 'Tied!'
+                elif machine_choice == 'Paper':
+                    result = 'Paper > Rock! You lose!'
+                    self.decrement_lives()
+                else:
+                    result = 'Scissors < Rock! You win!'
+            elif choice == 'p':
+                print('You have chosen Paper!')
+                print("The machine's choice was {}!".format(machine_choice))
+                if machine_choice == "Paper":
+                    result = 'Tied!'
+                elif machine_choice == 'Rock':
+                    result = 'Paper > Rock! You win!'
+                else:
+                    result = 'Scissors > Paper! You lose!'
+                    self.decrement_lives()
+            elif choice == 's':
+                print('You have chosen Scissors!')
+                print("The machine's choice was {}!".format(machine_choice))
+                if machine_choice == "Scissors":
+                    result = 'Tied!'
+                elif machine_choice == 'Paper':
+                    result = 'Paper < Scissors! You win!'
+                else:
+                    result = 'Scissors < Rock! You lose!'
+                    self.decrement_lives()
+            else:
+                app_log.warn("User chose a non supported choice: {}"
+                             .format(choice))
+                print('Your 3 choices are: r, s, p!')
+            trace_string = f'{number_choice} machinechoice is {machine_choice}'
+            result_string = f'. Result: {result}'
+            ''.join(trace_string, result_string)
+            app_log.trace(trace_string)
+            print()
+            print('Lives: {}'.format(self.lives))
+            number_choice += 1
+
+        app_log.trace("Game ended")
+        print('You have lost all your lifes! Come play other time!')
---
+++ days/34-36-refactoring/program.py
@@ -0,0 +1,32 @@
+import game
+import logbook
+import sys
+
+def init_logging(filename: str = None):
+        level = logbook.TRACE
+
+        if filename:
+            logbook.TimedRotatingFileHandler(filename, level=level).push_application()
+        else:
+            logbook.StreamHandler(sys.stdout, level=level).push_application()
+            
+        msg = 'Logging initialized, level: {}, mode: {}'.format(
+            level,
+            "stdout mode" if not filename else 'file mode: ' + filename
+        )
+        logger = logbook.Logger('Startup')
+        logger.notice(msg)
+
+if __name__ == '__main__':
+    init_logging("first_log.log")
+    game = game.Game()
+    game.main()
+
+    # print("Which game wanna play? Normal or 15")
+    # input_user = input("- ")
+    # if input_user == 'Normal':
+    #     game = game.Game()
+    #     game.main()
+    # else:
+    #     game = game_15.Game()
+    #     game.main()
