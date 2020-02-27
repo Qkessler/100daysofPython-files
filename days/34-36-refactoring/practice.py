2193177b32af146f8af99a469779a74324bcd753
Enrique Kessler <qkessler@gmail.com>
2020-02-27 11:23:40 +0100

    Refactoring other files.

--- days/34-36-refactoring/practice.py
+++ days/34-36-refactoring/practice.py
@@ -1,6 +1,4 @@
 
-
-
 dic = {}
 personas = 'Quique Elena David Lara'.split()
 años = '20 19 16 12'.split()
@@ -9,8 +7,10 @@ dic = dict(zip(personas, años))
 
 # La mejor forma de acceder evitando if -else es con un diccionario.
 
+
 def get_edad(persona):
     return dic.get(persona)
+
 
 persona = 'Quique'
 print(f'{persona} tiene {get_edad(persona)} años')
