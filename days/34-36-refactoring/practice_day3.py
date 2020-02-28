fc187e36b280739af63c5747ea0c36deadd23fd9
Qkessler <qkessler@gmail.com>
2020-02-28 08:43:38 +0100

    Adding join to the party.

--- days/34-36-refactoring/practice_day3.py
+++ days/34-36-refactoring/practice_day3.py
@@ -1,6 +1,11 @@
 
+personas = 'Mamá Papá David Quique'
+familia = personas.split()
+años_str = '54,50,66,20'
+años = años_str.split(',')
+diccionario_familia = dict(zip(familia, años))
+print(diccionario_familia)
 
-familia = 'Mamá Papá David Quique'.split()
 for i, persona in enumerate(familia, 1):
     print(f'Miembro {i}: {persona}')
 
@@ -16,8 +21,21 @@ Miembro 4: Quique
 # Para tener cuidado con los ficheros cuando los abres para cerrarlos con
 # cuidado de posibles excepciones.
 
+'''
 with open('practice.py', 'w') as f:
     f.write("Lo que sea")
+'''
 
 # Lo bueno es que with lo cierra automáticamente.
 
+print(min(diccionario_familia, key=lambda x: x[1]))
+
+# Concatenamos de forma más eficiente con la función de stdlib 'join'.
+
+print(':'.join(familia))
+
+'''
+La salida es:
+
+Mamá:Papá:David:Quique
+'''
