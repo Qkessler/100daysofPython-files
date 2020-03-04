c000be04adbb8fecacec91a8852bb2abad5fcf06
Qkessler <qkessler@gmail.com>
2020-03-04 09:30:11 +0100

    Commiting start of practice_day2

--- days/40-42-json-data/practice/practice_day2.py
+++ days/40-42-json-data/practice/practice_day2.py
@@ -5,12 +5,13 @@ import re
 
 
 API_key = 'e359a9fac7b8b71403adda4610d336c3c7707873'
-# URL = 'https://public-api.quickfs.net/v1/data/EPD/cf_cfo?api_key='
+URL = 'https://public-api.quickfs.net/v1/data/EPD/cf_cfo?api_key='
 URL_basic = 'https://public-api.quickfs.net/v1/data/'
 METRICS = ' https://public-api.quickfs.net/v1/metrics'
 # companies = ' https://public-api.quickfs.net/v1/companies&api_key=e359a9fac7b8b71403adda4610d336c3c7707873'
 URL_withAPI = URL + API_key
 # r = requests.get(METRICS)
+r = requests.get(URL_withAPI)
 data = json.loads(r.text)
 
 def get_metrics():
@@ -30,13 +31,13 @@ def get_metrics():
 
 # TODO: try to get 10 cap data out of API, and get a list of the companies.
 
-def get_10_cap_data(data, API_key):
-    URL_10cap = 
-    URL_request = URL_basic
+# def get_10_cap_data(data, API_key):
+#     URL_10cap = 
+#     URL_request = URL_basic
 
 
 if __name__ == '__main__':
     with open('metrics_file.txt', 'w') as f:
-        f.write(get_metrics())
+        f.write(str(get_metrics()))
     exit(0)
     print(get_10_cap_data())
