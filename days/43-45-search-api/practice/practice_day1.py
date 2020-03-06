75304a43f9cdad337d24497317fa546a538b0e96
Qkessler <qkessler@gmail.com>
2020-03-06 18:03:46 +0100

    Commiting practice_day1

---
+++ days/43-45-search-api/practice/practice_day1.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+# For the next days I'll be working with the Santander API, which also makes sense, as I will be using it for my personal project.
+
+# The API's name is Accounts v2. I couldn't log in on day 1, so I will keep working on quickfs API.
+
+def pre_step():
+    url =
+
