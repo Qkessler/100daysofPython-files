41c57a75ffecb596311fe120e2eb438e9a99e9f9
Enrique Kessler Martínez <qkessler@gmail.com>
2020-03-09 08:26:50 +0100

    Adding first test on bs4 scraper

--- days/46-48-beautifulsoup4/practice/practice_day1/scraper.py
+++ days/46-48-beautifulsoup4/practice/practice_day1/scraper.py
@@ -1,3 +1,22 @@
 import requests
 import bs4
 
+def pull_site(URL):
+    r = requests.get(URL)
+    r.raise_for_status()
+    return r
+
+def get_data(site):
+    holdings = []
+    scraper = bs4.BeautifulSoup(site.text, 'html.parser')
+    data = scraper.select('.holding-code')
+    for holding in data:
+        holdings.append(holding.getText())
+    for holding in holdings:
+        print(holding)
+    
+
+if __name__ == '__main__':
+    site = pull_site('https://portfolio.sharesight.com/portfolios/470306')
+    get_data(site)
+    
