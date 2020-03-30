96ee0b00945be80995902575b06ab3f847c7422a
Qkessler <qkessler@gmail.com>
2020-03-30 08:47:07 +0200

    Initial files

---
+++ days/67-69-pyperclip/practice/practice_day1.py
@@ -0,0 +1,12 @@
+import pyperclip
+
+
+# Tenemos varias funciones que vamos a usar:
+
+# pyperclip.paste()
+# pyperclip.copy(whatever : str)
+
+
+
+if __name__ == '__main__':
+    print(pyperclip.paste())
