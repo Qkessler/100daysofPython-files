e9c69f8954e1fb8cc46e496ef7ecdd5fe64ba688
Qkessler <qkessler@gmail.com>
2020-04-04 09:41:38 +0200

    Working script, got the books api data in the spreadsheet

--- days/70-72-openpyxl-excel-automation/practice/practice_day3.py
+++ days/70-72-openpyxl-excel-automation/practice/practice_day3.py
@@ -1,6 +1,8 @@
 from openpyxl import load_workbook
 import os
 import requests
+from typing import Dict
+from collections import namedtuple
 from pprint import pprint
 
 # I'm gonna try to get data from the APIs we have been working on and
@@ -10,6 +12,7 @@ API_LINK = '&key=' + BOOKS_KEY
 BASE_LINK = 'https://www.googleapis.com/books/v1/volumes?q='
 WB = load_workbook('books.xlsx')
 WS = WB['api']
+Book = namedtuple('Book', 'A B C D E F G')
 
 
 # I'm getting the basic volumes from the google Books API, based on a
@@ -19,9 +22,32 @@ def get_data(term):
     data = requests.get(request)
     json_data = data.json()
     items = json_data['items']
-    for i in items:
-        pprint(i)
-        break
+    counter = 2
+    for info in items:
+        volume_info = info['volumeInfo']
+        book = create_row(volume_info)
+        insert_row(book, counter)
+        counter += 1
+
+
+def create_row(volume_info: Dict):
+    book = Book(A=volume_info['title'], B=volume_info['authors'],
+                C=volume_info['language'], D=volume_info['categories'],
+                E=volume_info['pageCount'], F=volume_info['printType'],
+                G=volume_info['publishedDate'])
+    pprint(book)
+    return book
+
+
+def insert_row(book: Book, counter):
+    WS['A'+str(counter)] = str(book.A)
+    WS['B'+str(counter)] = str(book.B)
+    WS['C'+str(counter)] = str(book.C)
+    WS['D'+str(counter)] = str(book.D)
+    WS['E'+str(counter)] = str(book.E)
+    WS['F'+str(counter)] = str(book.F)
+    WS['G'+str(counter)] = str(book.G)
+    WB.save('books.xlsx')
 
 
 if __name__ == '__main__':
