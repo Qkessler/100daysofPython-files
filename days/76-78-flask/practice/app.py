3d3a248ff683208d81dc4d66ac20c8aaf0f700a3
Qkessler <qkessler@gmail.com>
2020-04-10 09:35:57 +0200

    Finished practice on day3, getting a book search on google books api done

--- days/76-78-flask/practice/app.py
+++ days/76-78-flask/practice/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, render_template
-from data import trabajo
 import os
 import requests
 from typing import Dict
@@ -8,43 +7,40 @@ from collections import namedtuple
 BOOKS_KEY = os.environ['BOOKS_KEY']
 API_LINK = '&key=' + BOOKS_KEY
 BASE_LINK = 'https://www.googleapis.com/books/v1/volumes?q='
-Book = namedtuple('Book', 'A B C D E F G')
+Book = namedtuple('Book', 'A B C E F G')
 app = Flask(__name__)
-books = []
-text = ""
 
 
 def get_data(term):
+    books = []
     request = BASE_LINK + term + API_LINK
     data = requests.get(request)
     json_data = data.json()
     items = json_data['items']
-    counter = 2
     for info in items:
         volume_info = info['volumeInfo']
         book = create_book(volume_info)
         books.append(book)
-        counter += 1
+    return books
 
 
 def create_book(volume_info: Dict):
     book = Book(A=volume_info['title'], B=volume_info['authors'],
-                C=volume_info['language'], D=volume_info['categories'],
+                C=volume_info['language'],
                 E=volume_info['pageCount'], F=volume_info['printType'],
                 G=volume_info['publishedDate'])
     return book
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', trabajo=trabajo)
+    if request.method == "GET":
+        return render_template('index.html')
+    else:
+        term = request.form['term']
+        books = get_data(term)
+        return render_template('search.html', term=term, books=books)
 
-
-@app.route('/', methods=['POST'])
-def input_box():
-    text = request.form['u']
-    return text
 
 if __name__ == '__main__':
-    get_data(text)
     app.run()
