3c60bae6290e596579001a62425555181d57bc66
Qkessler <qkessler@gmail.com>
2020-04-16 08:41:28 +0200

    Adding some progress

--- days/82-84-dataviz-plotly/practice/covid-19-analysis.py
+++ days/82-84-dataviz-plotly/practice/covid-19-analysis.py
@@ -1,10 +1,12 @@
-import plotly.express as px
+import plotly.graph_objects as go
 import csv
 from collections import namedtuple
 
 # This is a list of the thinks I could do to the data:
 # TODO: Get a plot of the result of the age of the patient
-#       and the respiratory syncytial virus. I'm trying to use a histogram.
+#       and the sars_cov_exam. The idea is to get the number
+#       with and without it per age. Maybe get two histograms
+#       one with and one without.
 
 
 name_file = 'diagnosis-of-covid-19-and-its-clinical-spectrum.csv'
@@ -31,6 +33,22 @@ def create_case(row):
     return case
 
 
+def transpose_tuples(data):
+    if isinstance(data, dict):
+        data = data.items()
+    transposed = list(zip(*data))
+    return transposed
+
+
+def data_age_syncytial():
+    data_dict = {case.patient_age:case.respiratory_syncytial_virus
+                 for case in data if case.respiratory_syncytial_virus}
+    return transpose_tuples(data_dict)
+
+
 if __name__ == '__main__':
     init()
-    fig = px.histogram(data, x="")
+    x, y = data_age_syncytial()
+    print(x, y)
+    fig = go.Figure(data=[go.Bar(x=x, y=y)])
+    fig.show()
